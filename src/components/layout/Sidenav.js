502c52f79267c3df2a7f30c4f2ba7dad7d097ef1
Cristofer <cristofer.russo94@gmail.com>
2023-03-15 12:53:58 +0100

    logo

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -86,24 +86,7 @@ function Sidenav({ color }) {
     </svg>,
   ];
 
-  const rtl = [
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      key={0}
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M3 6C3 4.34315 4.34315 3 6 3H16C16.3788 3 16.725 3.214 16.8944 3.55279C17.0638 3.89157 17.0273 4.29698 16.8 4.6L14.25 8L16.8 11.4C17.0273 11.703 17.0638 12.1084 16.8944 12.4472C16.725 12.786 16.3788 13 16 13H6C5.44772 13 5 13.4477 5 14V17C5 17.5523 4.55228 18 4 18C3.44772 18 3 17.5523 3 17V6Z"
-        fill={color}
-      ></path>
-    </svg>,
-  ];
-
+  
   const profile = [
     <svg
       width="20"
@@ -162,9 +145,8 @@ function Sidenav({ color }) {
 
   return (
     <>
-      <div className="brand">
-        <img src={logo} alt="" />
-        <span>Muse Dashboard</span>
+      <div>        
+        <img src={logo} alt="" style={{height: 160, marginLeft: 20 }} />
       </div>
       <hr />
       <Menu theme="light" mode="inline">
