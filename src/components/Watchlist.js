95343fb8ef4c3803fea99321d03f88b51ac1e898
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-22 16:58:10 +0100

    edit add to watchlist to pull data from database; add conditional rendering of table

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,11 +1,9 @@
 import React, {useState} from 'react'
 import WatchlistCoin from '../watchlistClassConstructor';
-import { useApi } from '../hooks/useApi';
 
-import { Button, Table, Row, Col, Space } from "antd";
+import { Table, Row, Col, Space } from "antd";
 
 export default function Watchlist(props) {
-    const { handleAllCoins } = useApi();
         
     const [watchlistCoins, setWatchlistCoins] = useState([])
 
@@ -13,19 +11,12 @@ export default function Watchlist(props) {
         const clickedCoin = event.target.id
         if(watchlistCoins.some(coin => coin.id === clickedCoin)){
             console.log("already watchlisted")
-            // add some sort of alert to user
+            // switch between filled/unfilled star
         } else {
-            handleAllCoins().then((res) => {
-                const coinData = res.find(coin => clickedCoin === coin.id)
-                let newCoin = new WatchlistCoin(coinData)
-                setWatchlistCoins(prevCoins => ([...prevCoins, newCoin]))
-            
-            /* ideally I wouldn't fetch the data from the API and instead get it from props.coins,
-                which I have passed down but couldn't make work because of state/re-renders */
-            //   const coinData = res.find(coin => clickedCoin === coin.id)
-            //   const newCoin = new WatchlistCoin(coinData)
-            //   setWatchlistCoins(prevCoins => ([...prevCoins, newCoin]))
-                })
+              const coinData = props.coins.find(coin => clickedCoin === coin.id)
+              const newCoin = new WatchlistCoin(coinData)
+              console.log(newCoin)
+              setWatchlistCoins(prevCoins => ([...prevCoins, newCoin]))
         }
     }
 
@@ -45,7 +36,7 @@ export default function Watchlist(props) {
             dataIndex: 'name',
             key: 'name',
             render: (text, record) =>   (<Space size="middle">
-                                            <img src={record.image} style={{width: '25px'}} alt='coin symbol'/>
+                                            <img src={record.image} style={{minWidth: '25px', maxWidth: '25px'}} alt='coin symbol'/>
                                             <span>{text} ({record.symbol})</span>
                                         </Space>),
         },
@@ -81,14 +72,17 @@ export default function Watchlist(props) {
     return (
         <div>
             {/* Button included for testing purposes */}
-            <Button id="bitcoin" type="primary" danger onClick={(event)=> addCoinToWatchlist(event)}>
+            <button id="bitcoin" type="primary" danger onClick={(event)=> addCoinToWatchlist(event)}>
             +
-            </Button>
-            <Row gutter={[24, 0]}>
-                <Col xs={24} sm={24} md={24} lg={24} xl={24} className="mb-24">
-                    <Table className="watchlist-table" columns={columns} dataSource={watchlistCoins} />
-                </Col>
-            </Row>
+            </button>
+
+            {watchlistCoins?.length > 0 ? 
+                <Row gutter={[24, 0]}>
+                    <Col xs={24} sm={24} md={24} lg={24} xl={24} className="mb-24">
+                        <Table className="watchlist-table" columns={columns} dataSource={watchlistCoins} />
+                    </Col>
+                </Row>
+            : null}
         </div>
     )
 }
