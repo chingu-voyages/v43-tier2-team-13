5a8111b1420c547ed7a9d7c27e3ca3ad3f61a9a7
Timothy Russo <russotimothy@live.it>
2023-04-05 15:58:07 +0200

    feat: make the github logo responsive

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,15 +1,3 @@
-/*!
-  =========================================================
-  * Muse Ant Design Dashboard - v1.0.0
-  =========================================================
-  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-  * Coded by Creative Tim
-  =========================================================
-  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { Layout, Row, Col } from 'antd';
 import { HeartFilled } from '@ant-design/icons';
 import { GithubOutlined } from '@ant-design/icons';
@@ -20,8 +8,11 @@ function Footer() {
   return (
     <AntFooter style={{ background: '#fafafa' }}>
       <Row className="just">
-        <Col xs={24} md={12} lg={12}>
-          <div className="copyright">
+        <Col xs={24} md={24} lg={24}>
+          <div
+            className="copyright"
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
             © 2023, made with
             {<HeartFilled />} by
             <a href="#pablo" className="font-weight-bold" target="_blank">
@@ -30,11 +21,15 @@ function Footer() {
             for a better web.
             <a
               href="https://github.com/chingu-voyages/v43-tier2-team-13"
-              style={{ cursor: 'pointer', marginLeft: '30px' }}
+              style={{
+                cursor: 'pointer',
+                marginLeft: 'auto',
+                marginRight: '20px',
+              }}
               target="_blank"
               rel="noreferrer"
             >
-              {<GithubOutlined />}
+              {<GithubOutlined style={{ fontSize: '18px' }} />}
             </a>
           </div>
         </Col>
